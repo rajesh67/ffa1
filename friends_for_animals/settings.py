aaddf9ab52d27594f2f8412337570ca10c8eaf4d
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-01-08 23:39:38 +0530

    Version-1.0

--- friends_for_animals/settings.py
+++ friends_for_animals/settings.py
@@ -128,23 +128,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/customs")
-]
-
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-MEDIAFILES_DIRS = [
-    os.path.join(BASE_DIR, "media/images")
-]
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
