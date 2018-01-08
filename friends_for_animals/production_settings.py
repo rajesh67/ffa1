1519fda6eb97e64614a4b19a014e96e8f6c016cf
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-01-08 23:48:04 +0530

    Version-1.0

--- friends_for_animals/production_settings.py
+++ friends_for_animals/production_settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 MEDIAFILES_DIRS = (
     os.path.join(BASE_DIR, "media/images"),
