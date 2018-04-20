c9165218f68b0208499cd83402ba1d4b0473b71a
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-04-20 13:29:23 +0530

    new ffa web app

--- friends_for_animals/urls.py
+++ friends_for_animals/urls.py
@@ -43,4 +43,8 @@ urlpatterns = [
     url(r'^blogs/', include('blogs.urls'), name="blogs"),
 
     url(r'^summernote/', include('django_summernote.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
